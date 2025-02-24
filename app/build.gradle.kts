c220bdd509246ae0861eb2a9e74acf5005092759
Govi <govidivi.1991@gmail.com>
2025-02-24 22:21:56 +0530

    chore: Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.govi.todoapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.govi.todoapp"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        packaging.resources.excludes.add("META-INF/gradle/incremental.annotation.processors")
+        packaging.resources.excludes.add("META-INF/NOTICE.md")
+    }
+    buildFeatures{
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.bundles.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.bundles.unit.tests)
+}
---
+++ app/src/main/java/com/govi/todoapp/TodoApplication.kt
@@ -0,0 +1,8 @@
+package com.govi.todoapp
+
+import android.app.Application
+
+/**
+* Created by Govi on 24,February,2025
+*/
+class TodoApplication : Application()
